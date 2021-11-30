444bde15449583d0d502f60cca6c431eda2ecd74
Nattawipa Saetae <55126671+Nattawipa@users.noreply.github.com>
2021-12-01 00:37:38 +0700

    lab 11 - 1

---
+++ Lab7_A1/config.h
@@ -0,0 +1,10 @@
+/* NetPie MQTT Server */
+const char* mqttServer = "broker.netpie.io";
+const int mqttPort = 1883;
+const char* mqttClient = "46ac06a6-bce6-48af-8605-4238e3e1eeb3"; //Client ID
+const char* mqttUser = "iosgvE1fgTJNRsdBVNHLc3Xbsyc5QcjX"; //Token
+const char* mqttPassword = "S4TkMhhjdJ6PuJePhQJd6F)nwTVoXfar"; //Secret
+
+/* (WiFi) Variables */
+char ssid[] = "Teerasak 2.4 G"; // Your WiFi credentials.
+char pass[] = "click123"; // Set password to "" for open networks.
